4dcb5553b2feff1eaa722dac631c15fa71bfaa86
rkr-dev <object.prototype.js@gmail.com>
2021-05-13 10:04:39 -0400

    Banner Section
    Banner Section

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -4,10 +4,27 @@ import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
 import BannerImg from 'assets/banner-thumb.png';
 import ShapeLeft from 'assets/shape-left.png';
 import ShapeRight from 'assets/shape-right.png';
+import Head from 'next/head';
 
 export default function Banner() {
   return (
-    <h1>Banner</h1>
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            Top Quality Digital Products to Explore
+            </Heading>
+          <Text as="p" variant="heroSecondary">
+            Checkout our website to find great software products and deals! If you need a website or a web application
+            this is a place to go
+          </Text>
+          <Button variant="primary">Explore</Button>
+        </Box>
+        <Box sx={styles.banner.imageBox}>
+          <Image src={ BannerImg} alt="banner"/>
+        </Box>
+      </Container>
+    </section>
   );
 }
 
