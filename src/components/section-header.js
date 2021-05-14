b809392ea644318d3abe680a8e6b1443c176ef8c
rkr-dev <object.prototype.js@gmail.com>
2021-05-13 21:51:53 -0400

    Banner and Key Features
    - Banner Section
    - Key Features

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -2,7 +2,25 @@ import React from 'react';
 import { Box, Text, Heading } from 'theme-ui';
 
 export default function SectionHeader({ title, slogan, isWhite }) {
-  return (
-    <h1>SectionHeader</h1>
-  );
+  return <Box sx={{ variant: 'sectionHeader' }}>
+    <Text
+      as="p"
+      sx={{
+        variant: 'sectionHeader.subTitle',
+        color: isWhite ? 'white' : 'primary',
+        opacity:isWhite?'0.7':1
+      }}
+    >
+      {slogan}
+    </Text>
+    <Heading
+          as="p"
+          sx={{
+            variant: 'sectionHeader.title',
+            color: isWhite ? 'white' : 'heading',
+          }}
+    >
+      {title}
+    </Heading>
+  </Box>;
 }
